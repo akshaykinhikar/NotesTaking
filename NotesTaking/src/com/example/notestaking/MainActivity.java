847f0a33c2db4a1afee174b4f6b15b5f0f81f72f
akshay <kinhikar.akshay32@gmail.com>
2014-10-16 13:05:21 +0530

    -Date and time of notes is displayed in log

--- NotesTaking/src/com/example/notestaking/MainActivity.java
+++ NotesTaking/src/com/example/notestaking/MainActivity.java
@@ -1,15 +1,29 @@
 package com.example.notestaking;
 
+import java.util.List;
+
+import com.example.notestaking.data.NoteItem;
+import com.example.notestaking.data.NotesDataSource;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
-
+	private NotesDataSource datasource;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		datasource = new NotesDataSource();
+		List<NoteItem> notes = datasource.findAll();
+		NoteItem note = notes.get(0);
+		 
+		Log.i("Notes", note.getKey());
+		
+	
 	}
 
 	@Override
