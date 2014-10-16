4da09832f91f6c745e506ed4c3884730d65a8f0d
akshay <kinhikar.akshay32@gmail.com>
2014-10-16 13:13:04 +0530

    -Created instance of SharedPreferences

--- NotesTaking/src/com/example/notestaking/MainActivity.java
+++ NotesTaking/src/com/example/notestaking/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		datasource = new NotesDataSource();
+		datasource = new NotesDataSource(this);
 		List<NoteItem> notes = datasource.findAll();
 		NoteItem note = notes.get(0);
 		 
--- NotesTaking/src/com/example/notestaking/data/NotesDataSource.java
+++ NotesTaking/src/com/example/notestaking/data/NotesDataSource.java
@@ -3,7 +3,17 @@ package com.example.notestaking.data;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 public class NotesDataSource {
+	private static final String PREFKEY = "notes";
+	private SharedPreferences notePrefs;
+	
+	public NotesDataSource(Context context){
+		notePrefs = context.getSharedPreferences(PREFKEY, context.MODE_PRIVATE);
+		
+	}
 	
 	public List<NoteItem> findAll(){
 		
