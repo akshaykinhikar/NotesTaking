847ada37392c09305e371baaafb6c67e2c01faee
akshay <kinhikar.akshay32@gmail.com>
2014-10-17 12:34:31 +0530

    Linked the Add button to open new Activity

--- NotesTaking/src/com/example/notestaking/MainActivity.java
+++ NotesTaking/src/com/example/notestaking/MainActivity.java
@@ -7,8 +7,10 @@ import com.example.notestaking.data.NotesDataSource;
 
 import android.os.Bundle;
 import android.app.ListActivity;
+import android.content.Intent;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 
 public class MainActivity extends ListActivity {
@@ -43,4 +45,23 @@ public class MainActivity extends ListActivity {
 		return true;
 	}
 
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+
+		if (item.getItemId() == R.id.action_create) {
+			createNote();
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	private void createNote() {						//Creates the new note
+
+		NoteItem note =NoteItem.getNew();		//new obj declared
+		Intent intent = new Intent(this,NoteEditorActivity.class);  //navigate between Screen
+		intent.putExtra("key", note.getKey());	//passed string
+		intent.putExtra("text", note.getText());
+		startActivityForResult(intent, 1001); //start Activity
+	
+	
+	}
 }
--- NotesTaking/src/com/example/notestaking/NoteEditorActivity.java
+++ NotesTaking/src/com/example/notestaking/NoteEditorActivity.java
@@ -1,13 +1,28 @@
 package com.example.notestaking;
 
+import com.example.notestaking.data.NoteItem;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class NoteEditorActivity extends Activity {
 
+	private NoteItem note;		
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_note_editor);
+	
+		Intent intent = this.getIntent(); //get reference to intent in main Activity
+		note = new NoteItem(); 	//instantiate note item
+		note.setKey(intent.getStringExtra("key")); //get the value of key and set
+		note.setText(intent.getStringExtra("text"));
+		
+		EditText et =(EditText) findViewById(R.id.noteText);
+		et.setText(note.getText());
+		et.setSelection(note.getText().length());
+		
 	}
 }
