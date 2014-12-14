899486e796b838ac2e10201c80d773ee80b2099e
akshay <kinhikar.akshay32@gmail.com>
2014-12-14 19:15:57 +0530

    -Update crash error resolved

--- NotesTaking/src/com/example/notestaking/MainActivity.java
+++ NotesTaking/src/com/example/notestaking/MainActivity.java
@@ -18,7 +18,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends ListActivity {
-	private static final int Editor_Activity_request = 1001;
+	private static final int EDITOR_ACTIVITY_REQUEST = 1001;
 	private static final int Menu_Delete_Id = 1002;
 	private int currentNoteId;
 	
@@ -38,8 +38,9 @@ public class MainActivity extends ListActivity {
 	private void refreshDisplay() {
 		notesList = datasource.findAll();
 		ArrayAdapter<NoteItem> adapter =
-				new ArrayAdapter<NoteItem>(this, R.layout.list_item_layout,notesList);
+				new ArrayAdapter<NoteItem>(this, R.layout.list_item_layout, notesList);
 		setListAdapter(adapter);
+		
 	}
 
 	@Override
@@ -48,38 +49,39 @@ public class MainActivity extends ListActivity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
+	
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 
 		if (item.getItemId() == R.id.action_create) {
 			createNote();
 		}
+		
 		return super.onOptionsItemSelected(item);
 	}
 
-	private void createNote() {						//Creates the new note
-
-		NoteItem note =NoteItem.getNew();		//new obj declared
-		Intent intent = new Intent(this,NoteEditorActivity.class);  //navigate between Screen
-		intent.putExtra("key", note.getKey());	//passed string
+	private void createNote() {
+		NoteItem note = NoteItem.getNew();
+		Intent intent = new Intent(this, NoteEditorActivity.class);
+		intent.putExtra("key", note.getKey());
 		intent.putExtra("text", note.getText());
-		startActivityForResult(intent, Editor_Activity_request); //start Activity
-
+		startActivityForResult(intent, EDITOR_ACTIVITY_REQUEST);
 	}
 
 	@Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
+		
 		NoteItem note = notesList.get(position);
 		Intent intent = new Intent(this, NoteEditorActivity.class);
 		intent.putExtra("key", note.getKey());
-		intent.putExtra("key", note.getText());
-		startActivityForResult(intent, Editor_Activity_request);
+		intent.putExtra("text", note.getText());
+		startActivityForResult(intent, EDITOR_ACTIVITY_REQUEST);
+		
 	}
-
+	
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		if(requestCode == Editor_Activity_request && resultCode == RESULT_OK ){
+		if (requestCode == EDITOR_ACTIVITY_REQUEST && resultCode == RESULT_OK) {
 			NoteItem note = new NoteItem();
 			note.setKey(data.getStringExtra("key"));
 			note.setText(data.getStringExtra("text"));
@@ -87,26 +89,26 @@ public class MainActivity extends ListActivity {
 			refreshDisplay();
 		}
 	}
-
+	
 	@Override
 	public void onCreateContextMenu(ContextMenu menu, View v,
 			ContextMenuInfo menuInfo) {
 		
 		AdapterContextMenuInfo info = (AdapterContextMenuInfo) menuInfo;
-		currentNoteId = (int) info.id;
-		menu.add(0,Menu_Delete_Id,0,"Delete");
-		
+		currentNoteId = (int)info.id;
+		menu.add(0, Menu_Delete_Id, 0, "Delete");
 	}
-
-		@Override
-		public boolean onContextItemSelected(MenuItem item) {
-			
-			if (item.getItemId() == Menu_Delete_Id) {
-				NoteItem note = notesList.get(currentNoteId);
-				datasource.remove(note);
-				refreshDisplay();
-				
-			}
-			return super.onContextItemSelected(item);
+	
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		
+		if (item.getItemId() == Menu_Delete_Id) {
+			NoteItem note = notesList.get(currentNoteId);
+			datasource.remove(note);
+			refreshDisplay();
 		}
+		
+		return super.onContextItemSelected(item);
+	}
+	
 }
